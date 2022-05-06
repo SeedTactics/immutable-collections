3716bca469af61ead2791a203553e0148801a919
John Lenz <wuzzeb@gmail.com>
2022-05-06 13:27:29 -0500

    temporarily restore hashing into imap.ts

--- src/imap.ts
+++ src/imap.ts
@@ -1,7 +1,13 @@
-import { HashKey } from "./hashing";
 import { LazySeq } from "./lazyseq";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/no-var-requires
 const hamt: any = require("hamt_plus");
+
+export type HashKeyObj = {
+  equals(other: unknown): boolean;
+  hashPrimitives(): ReadonlyArray<HashKey | Date | null | undefined>;
+};
+
+export type HashKey = string | number | boolean | HashKeyObj;
 
 export interface IMap<K, V> {
   isEmpty(): boolean;
@@ -29,7 +35,6 @@ export interface IMap<K, V> {
   collectValues(f: (v: V, k: K) => V | null | undefined): IMap<K, V>;
 }
 
-/*
 interface MakeConfig<K> {
   readonly keyEq: (a: K, b: K) => boolean;
   readonly hash: (v: K) => number;
@@ -40,6 +45,154 @@ interface HamtMap<K, V> extends IMap<K, V> {
   endMutation(): HamtMap<K, V>;
 
   _config: MakeConfig<K>;
+}
+
+function isHashKeyObj(k: unknown): k is HashKeyObj {
+  return k !== null && typeof k === "object" && "hashPrimitives" in k && "equals" in k;
+}
+
+function primEq(a: unknown, b: unknown): boolean {
+  return a === b;
+}
+
+// hamt_plus uses the hash value in javascript bit operations, and javascript bit operations cast
+// the number to a signed 32-bit integer.  Thus each hash function should return a number which
+// is within the range of a signed 32-bit integer.
+
+function hash2Ints(h1: number, h2: number): number {
+  // combines two 32-bit hashes into a 32-bit hash
+  return (h1 * 16777619) ^ h2;
+}
+
+function stringHash(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = hash2Ints(hash, str.charCodeAt(i));
+  }
+  return hash2Ints(hash, str.length);
+}
+
+function boolHash(a: boolean): number {
+  return a ? 1 : 0;
+}
+
+function numHash(a: number): number {
+  // numbers are in general a IEEE double
+  if (Number.isInteger(a) && a >= -2_147_483_648 && a <= 2_147_483_647) {
+    return a; // hash is just the number itself since it is a 32-bit signed integer
+  } else if (Object.is(a, Infinity) || Object.is(a, NaN)) {
+    return 0;
+  } else {
+    // convert the number to a 64-bit array and combine the two 32-bit halves
+    const buff = new ArrayBuffer(8);
+    new Float64Array(buff)[0] = a;
+    const intarr = new Int32Array(buff);
+    return hash2Ints(intarr[0], intarr[1]);
+  }
+}
+
+function objHash(a: HashKeyObj): number {
+  const prims = a.hashPrimitives();
+  let hash = 0;
+  for (let i = 0; i < prims.length; i++) {
+    const p = prims[i];
+    if (p === null || p === undefined) {
+      hash = hash2Ints(hash, 0);
+    } else {
+      switch (typeof p) {
+        case "string":
+          hash = hash2Ints(hash, stringHash(p));
+          break;
+        case "number":
+          hash = hash2Ints(hash, numHash(p));
+          break;
+        case "boolean":
+          hash = hash2Ints(hash, boolHash(p));
+          break;
+        default:
+          if (p instanceof Date) {
+            hash = hash2Ints(hash, p.getTime());
+          } else if (isHashKeyObj(p)) {
+            hash = hash2Ints(hash, objHash(p));
+          } else {
+            // typescript should prevent this from happening
+            hash = hash2Ints(hash, stringHash((p as unknown as object).toString()));
+          }
+          break;
+      }
+    }
+  }
+  return hash2Ints(hash, prims.length);
+}
+
+function makeWithDynamicConfig<K, V>(): HamtMap<K, V> {
+  // eslint-disable-next-line prefer-const
+  let m: HamtMap<K, V>;
+
+  // we have a small hack here.  At the time of creation, we don't know the
+  // key type of the map.  We only know the type the first time a key/value is
+  // inserted.  Therefore, for this initial empty map, we use a map configuration
+  // for the keyEq and hash function which check the type of the key and then
+  // replace the configuration with the correct one.  Technically the _config
+  // property is an internal property but this works and we are only changing the _config
+  // property once on the empty map and then never again.
+
+  function updateMapConfig(k: K): void {
+    switch (typeof k) {
+      case "object":
+        if (isHashKeyObj(k)) {
+          // the key types passed to _config.keyEq and _config.hash are equal
+          // to the type K which we just narrowed, but typescript doesn't know
+          // about the narrowing when typing keyEq and hash
+          m._config = {
+            keyEq: (j1, j2) => (j1 as unknown as HashKeyObj).equals(j2),
+            hash: objHash as unknown as (k: K) => number,
+          };
+          return;
+        } else {
+          throw new Error("key type must have equals and hash methods");
+        }
+
+      case "string":
+        m._config = {
+          keyEq: primEq,
+          // we just narrowed K to string, but typescript forgets this when
+          // typing hash
+          hash: stringHash as unknown as (k: K) => number,
+        };
+        return;
+
+      case "boolean":
+        m._config = {
+          keyEq: primEq,
+          hash: boolHash as unknown as (k: K) => number,
+        };
+        return;
+
+      case "number":
+        m._config = {
+          keyEq: primEq,
+          hash: numHash as unknown as (k: K) => number,
+        };
+        return;
+    }
+  }
+
+  function firstKeyEq(k1: K, k2: K): boolean {
+    updateMapConfig(k1);
+    return m._config.keyEq(k1, k2);
+  }
+  function firstHash(k: K): number {
+    updateMapConfig(k);
+    return m._config.hash(k);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  m = hamt.make({
+    keyEq: firstKeyEq,
+    hash: firstHash,
+  });
+  return m;
 }
 
 export function emptyIMap<K, V>(): IMap<K, V> {
@@ -132,6 +285,9 @@ function collectValuesIMap<K, V>(this: IMap<K & HashKey, V>, f: (v: V) => V | nu
   return m.endMutation();
 }
 
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const hamtProto = hamt.empty.prototype;
 if (hamtProto.toLazySeq === undefined) {
   hamtProto.toLazySeq = imapToLazySeq;
@@ -142,4 +298,3 @@ if (hamtProto.toLazySeq === undefined) {
   hamtProto.mapValues = mapValuesIMap;
   hamtProto.collectValues = collectValuesIMap;
 }
-*/
