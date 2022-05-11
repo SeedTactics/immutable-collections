30ff12c39f1ee7dd81c8812fd1d07e2964e90eba
John Lenz <wuzzeb@gmail.com>
2022-05-11 11:11:07 -0500

    hamt: some docs and cleanup

--- src/hamt.ts
+++ src/hamt.ts
@@ -1,18 +1,68 @@
 import { HashConfig } from "./hashing";
 
-export type LeafNode<K, V> = { hash: number; key: K; val: V };
-export type CollisionNode<K, V> = { hash: number; collision: Array<{ key: K; val: V }> };
-export type BitmapIndexedNode<K, V> = { bitmap: number; children: Array<HamtNode<K, V>> };
-export type FullNode<K, V> = { full: Array<HamtNode<K, V>> };
+/*
+A Hash Array Mapped Trie (HAMT)
+
+A HAMT is a tree in which each node has 32 children and the hash of the key is
+used to determine which child the entry is placed in.  To do so, the hash is
+split into 5-bit chunks.  The 5-bit value of each chunk (so 2^5 = 32 possible
+values) is used to determine which of the children the key will be placed in.
+
+Storing 32-children in a single node when many of them are empty or null is
+inefficient, so there is one enhancement.  For a node with some null/empty
+children, we instead store a 32-bit bitmap of the children that are non-null and
+then only store the non-null children in an array.
+
+For example, if we have a node with the following bitmap
+0b00000000_00001000_00100000_10001000 this means the node only has 4 non-null
+children and they are the positions of the 1s.  Only the four non-null children are
+stored and some fancy bit operations are used to determine which ones they are.
+
+The leaves are either a single leaf node with the key and the value or, if two
+keys hash to the same value, we store an collision array.
+*/
+
+// A leaf with the hash, key, and value.
+export type LeafNode<K, V> = { readonly hash: number; readonly key: K; readonly val: V };
+export type MutableLeafNode<K, V> = { readonly hash: number; readonly key: K; val: V };
+
+// A leaf node with the hash and an array of keys and values with this same hash.
+export type CollisionNode<K, V> = {
+  readonly hash: number;
+  readonly collision: ReadonlyArray<{ readonly key: K; readonly val: V }>;
+};
+export type MutableCollisionNode<K, V> = {
+  readonly hash: number;
+  readonly collision: Array<{ readonly key: K; val: V }>;
+};
+
+// A internal node with some null children.  Which children are non-null is specified in the bitmap and then
+// all non-null children are stored in an array.
+export type BitmapIndexedNode<K, V> = { readonly bitmap: number; readonly children: ReadonlyArray<HamtNode<K, V>> };
+export type MutableBitmapIndexedNode<K, V> = {
+  bitmap: number;
+  readonly children: Array<MutableHamtNode<K, V>>;
+};
+
+// A full node in which all 32 children are non-null
+export type FullNode<K, V> = { readonly full: ReadonlyArray<HamtNode<K, V>> };
+export type MutableFullNode<K, V> = { readonly full: Array<MutableHamtNode<K, V>> };
 
 export type HamtNode<K, V> =
-  | { empty: null }
+  | { readonly empty: null }
   | LeafNode<K, V>
   | CollisionNode<K, V>
   | BitmapIndexedNode<K, V>
   | FullNode<K, V>;
 
-export const empty: HamtNode<unknown, unknown> = { empty: null };
+export type MutableHamtNode<K, V> =
+  | { readonly empty: null }
+  | MutableLeafNode<K, V>
+  | MutableCollisionNode<K, V>
+  | MutableBitmapIndexedNode<K, V>
+  | MutableFullNode<K, V>;
+
+export const empty: { readonly empty: null } = { empty: null };
 
 const bitsPerSubkey = 5;
 const subkeyMask = (1 << bitsPerSubkey) - 1;
@@ -39,69 +89,6 @@ function sparseIndex(bitmap: number, mask: number): number {
 // index of the hash in a full node
 function fullIndex(hash: number, shift: number): number {
   return (hash >>> shift) & subkeyMask;
-}
-
-type BitmapNodeBeforeChildren<K, V> = {
-  bitmap: number;
-  children?: Array<BitmapNodeBeforeChildren<K, V> | LeafNode<K, V>>;
-};
-
-// create a new node consisting of two children. Requires that the keys and hashes of the keys are not equal
-function two<K, V>(shift: number, leaf1: LeafNode<K, V>, leaf2: LeafNode<K, V>): HamtNode<K, V> {
-  const hash1 = leaf1.hash;
-  const hash2 = leaf2.hash;
-  let root: BitmapNodeBeforeChildren<K, V> | undefined;
-  let parent: BitmapNodeBeforeChildren<K, V> | undefined;
-
-  do {
-    const mask1 = mask(hash1, shift);
-    const mask2 = mask(hash2, shift);
-
-    if (mask1 === mask2) {
-      // need to recurse
-      const newNode = { bitmap: mask1 };
-      if (root === undefined) {
-        root = newNode;
-      }
-      if (parent !== undefined) {
-        parent.children = [newNode];
-      }
-      parent = newNode;
-
-      shift = shift + bitsPerSubkey;
-    } else {
-      // we can insert
-      const newNode = { bitmap: mask1 | mask2, children: mask1 < mask2 ? [leaf1, leaf2] : [leaf2, leaf1] };
-      if (parent !== undefined) {
-        parent.children = [newNode];
-      }
-      // typescript doesn't know that we are guaranteed to have set children because if root has a value,
-      // root was also the parent at some point and had it's children set.  Therefore, cast root to BitmapIndexedNode<K, V>
-      return (root as BitmapIndexedNode<K, V>) ?? newNode;
-    }
-  } while (shift <= maxShift);
-
-  throw new Error("Internal violation: shift > maxShift for two " + JSON.stringify({ shift, leaf1, leaf2 }));
-}
-
-// create a new bitmap indexed or full node
-function bitmapIndexedOrFull<K, V>(bitmap: number, children: Array<HamtNode<K, V>>): HamtNode<K, V> {
-  if (children.length === maxChildren) {
-    return { full: children };
-  } else {
-    return { bitmap, children };
-  }
-}
-
-function copyAndSpliceArray<T>(arr: Array<T>, at: number, t: T): Array<T> {
-  const len = arr.length;
-  let i = 0;
-  let g = 0;
-  const out = new Array<T>(len + 1);
-  while (i < at) out[g++] = arr[i++];
-  out[at] = t;
-  while (i < len) out[++g] = arr[i++];
-  return out;
 }
 
 export function lookup<K, V>(cfg: HashConfig<K>, k: K, rootNode: HamtNode<K, V>): V | undefined {
@@ -148,6 +135,72 @@ export function lookup<K, V>(cfg: HashConfig<K>, k: K, rootNode: HamtNode<K, V>)
   } while (node);
 
   return undefined;
+}
+
+// a MutableBitmapNode but children property is not yet set so is undefined
+type MutableBitmapNodeBeforeChildren<K, V> = {
+  bitmap: number;
+  children?: Array<MutableBitmapNodeBeforeChildren<K, V> | LeafNode<K, V>>;
+};
+
+// create a new node consisting of two children. Requires that the keys and hashes of the keys are not equal
+function two<K, V>(shift: number, leaf1: LeafNode<K, V>, leaf2: LeafNode<K, V>): MutableHamtNode<K, V> {
+  const hash1 = leaf1.hash;
+  const hash2 = leaf2.hash;
+  let root: MutableBitmapNodeBeforeChildren<K, V> | undefined;
+  let parent: MutableBitmapNodeBeforeChildren<K, V> | undefined;
+
+  do {
+    const mask1 = mask(hash1, shift);
+    const mask2 = mask(hash2, shift);
+
+    if (mask1 === mask2) {
+      // need to recurse
+      const newNode = { bitmap: mask1 };
+      if (root === undefined) {
+        root = newNode;
+      }
+      if (parent !== undefined) {
+        parent.children = [newNode];
+      }
+      parent = newNode;
+
+      shift = shift + bitsPerSubkey;
+    } else {
+      // we can insert
+      const newNode = { bitmap: mask1 | mask2, children: mask1 < mask2 ? [leaf1, leaf2] : [leaf2, leaf1] };
+      if (parent !== undefined) {
+        parent.children = [newNode];
+      }
+      // typescript doesn't know that we are guaranteed to have set children because if root has a value,
+      // root was also the parent at some point and had it's children set.  Therefore, cast root to MutableBitmapIndexedNode<K, V>
+      return (root as MutableBitmapIndexedNode<K, V>) ?? newNode;
+    }
+  } while (shift <= maxShift);
+
+  throw new Error(
+    "Internal immutable-collections violation: shift > maxShift for two " + JSON.stringify({ shift, leaf1, leaf2 })
+  );
+}
+
+// create a new bitmap indexed or full node
+function bitmapIndexedOrFull<K, V>(bitmap: number, children: ReadonlyArray<HamtNode<K, V>>): HamtNode<K, V> {
+  if (children.length === maxChildren) {
+    return { full: children };
+  } else {
+    return { bitmap, children };
+  }
+}
+
+function copyAndSpliceArray<T>(arr: ReadonlyArray<T>, at: number, t: T): Array<T> {
+  const len = arr.length;
+  let i = 0;
+  let g = 0;
+  const out = new Array<T>(len + 1);
+  while (i < at) out[g++] = arr[i++];
+  out[at] = t;
+  while (i < len) out[++g] = arr[i++];
+  return out;
 }
 
 export function insert<K, V>(
@@ -227,12 +280,12 @@ export function mutateInsert<K, V>(
   cfg: HashConfig<K>,
   k: K,
   v: V,
-  rootNode: HamtNode<K, V>,
+  rootNode: MutableHamtNode<K, V>,
   merge: ((v1: V, v2: V) => V) | undefined
-): readonly [HamtNode<K, V>, boolean] {
+): readonly [MutableHamtNode<K, V>, boolean] {
   const hash = cfg.hash(k);
   let existing = false;
-  function loop(shift: number, node: HamtNode<K, V>): HamtNode<K, V> {
+  function loop(shift: number, node: MutableHamtNode<K, V>): MutableHamtNode<K, V> {
     if ("bitmap" in node) {
       const m = mask(hash, shift);
       const idx = sparseIndex(node.bitmap, m);
