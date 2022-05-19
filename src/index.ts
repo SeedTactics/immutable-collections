778bb8b125e5d52dc6e5050b8d8dafa4ab488a40
John Lenz <wuzzeb@gmail.com>
2022-05-19 14:51:45 -0500

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export type { HashKey, HashKeyObj } from "./hashing.js";
+export { ImMap } from "./immap.js";
+export { ImSet } from "./imset.js";
+export { LazySeq, PrimitiveOrd, ToPrimitiveOrd, SortByProperty, sortByProp } from "./lazyseq.js";
